5e71a98026f1c2718499c92faa9273f45f5c3e25
DPthang411 <dinhphuocthang46@gmail.com>
2024-07-28 17:13:51 +0700

    add codes

--- main.py
+++ main.py
@@ -27,13 +27,14 @@ class Bullet(Entity):
             destroy(hitinfo.entity)
 
 class Player(Entity):
-    def __init__(self):
+    def __init__(self, parent):
         super().__init__()
         self.model = 'quad'
         self.color = color.green
         self.collider = 'box'
         self.on_cooldown = False
         self.list = []
+        self.parent = parent
     def shoot(self):
         if self.on_cooldown == False:
             bullet = Bullet(self)
@@ -44,21 +45,23 @@ class Player(Entity):
         rot_mouse = atan2(mouse.x, mouse.y)
         self.rotation_z = degrees(rot_mouse) - 90
         if held_keys['left mouse']:
-            self.shoot()
+            if self.parent.enabled == True:
+                self.shoot()
 class Scene1(Entity):
     def __init__(self):
         super().__init__()
-        self.char = Player()
+        self.char = Player(self)
         self.bullets = self.char.list
         self.enemies = []
         self.create_enemies()
     def create_enemies(self):
-        x = random.choice((8,9,7,8.1,8.2,8.3,9.1,9.2,9.3,7.1,7.2,7.3,-8,-9,-7,-8.1,-8.2,-8.3,-9.1,-9.2,-9.3,-7.1,-7.2,-7.3))
-        y = random.uniform(-5,5)
-        enemy = Enemy(x, y)
-        enemy.add_script(SmoothFollow(target = self.char, speed = 0.5))
-        self.enemies.append(enemy)
-        invoke(self.create_enemies, delay = 3)
+        if self.enabled == True:
+            x = random.choice((8,9,7,8.1,8.2,8.3,9.1,9.2,9.3,7.1,7.2,7.3,-8,-9,-7,-8.1,-8.2,-8.3,-9.1,-9.2,-9.3,-7.1,-7.2,-7.3))
+            y = random.uniform(-5,5)
+            enemy = Enemy(x, y)
+            enemy.add_script(SmoothFollow(target = self.char, speed = 0.5))
+            self.enemies.append(enemy)
+            invoke(self.create_enemies, delay = 3)
     def destroy_bullet(self):
         for bullet in self.bullets:
             destroy(bullet)
@@ -67,7 +70,29 @@ class Scene1(Entity):
         for enemy in self.enemies:
             destroy(enemy)
         self.enemies.clear()
+class Retry(Button):
+    def __init__(self):
+        super().__init__()
+        self.model = 'quad'
+        self.text = 'Retry'
+        self.text_size = 10
+        self.position = (10,10)
+        self.color = color.red
+        self.scale = (0.1,0.1)
+def enabled():
+    scene1.enabled = True
+def update():
+    if type(scene1.char.intersects().entity) == Enemy:
+        scene1.enabled = False
+        scene1.destroy_bullet()
+        scene1.destroy_enemy()
+        retry.position = (0,0)
+    if retry.hovered == True:
+        if held_keys['left mouse']:
+            enabled()
+            retry.position = (10,10)
 
 app = Ursina()
+retry = Retry()
 scene1 = Scene1()
 app.run()
