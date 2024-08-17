54267700032cd1b61456349d26e476bc64829d35
DPthang411 <dinhphuocthang46@gmail.com>
2024-08-17 19:58:27 +0700

    add healthbar system

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from ursina import *
+from ursina.prefabs.health_bar import HealthBar
 from math import atan2, degrees
 import random
 class Enemy(Entity):
@@ -54,6 +55,7 @@ class Scene1(Entity):
         self.bullets = self.char.list
         self.enemies = []
         self.create_enemies()
+        self.healthbar = HealthBar(color = color.red, max_value=3)
     def create_enemies(self):
         if self.enabled == True:
             x = random.choice((8,9,7,8.1,8.2,8.3,9.1,9.2,9.3,7.1,7.2,7.3,-8,-9,-7,-8.1,-8.2,-8.3,-9.1,-9.2,-9.3,-7.1,-7.2,-7.3))
@@ -61,7 +63,7 @@ class Scene1(Entity):
             enemy = Enemy(x, y)
             enemy.add_script(SmoothFollow(target = self.char, speed = 0.5))
             self.enemies.append(enemy)
-            invoke(self.create_enemies, delay = 3)
+        invoke(self.create_enemies, delay = 2)
             
     def destroy_bullet(self):
         for bullet in self.bullets:
@@ -80,19 +82,24 @@ class Retry(Button):
         self.position = (10,10)
         self.color = color.red
         self.scale = (0.1,0.1)
-def enabled():
+def restart():
+    scene1.healthbar.value = 3
     scene1.enabled = True
+    retry.position = (10,10)
 def update():
     if type(scene1.char.intersects().entity) == Enemy:
-        scene1.enabled = False
-        scene1.destroy_bullet()
-        scene1.destroy_enemy()
-        retry.position = (0,0)
-    if retry.hovered == True:
-        if held_keys['left mouse']:
-            enabled()
-            retry.position = (10,10)
-
+        scene1.healthbar.value -= 1
+        hit = scene1.char.intersects().entity
+        if scene1.healthbar.value > 0:
+            destroy(scene1.char.intersects().entity)
+            scene1.enemies.remove(hit)
+        elif scene1.healthbar.value == 0:
+            scene1.enabled = False
+            scene1.destroy_bullet()
+            scene1.destroy_enemy()
+            retry.position = (0,0)
+            retry.on_click = restart
+                    
 app = Ursina()
 retry = Retry()
 scene1 = Scene1()
